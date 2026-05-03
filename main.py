0f2343a08e6cd7e3ae242c70059c3cd380854897
maaanulele <143810847+maaanulele@users.noreply.github.com>
2026-05-03 16:37:14 +0200

    Update main.py
    Data Base things

--- main.py
+++ main.py
@@ -4,6 +4,27 @@ import uuid
 from pathlib import Path
 import flet as ft
 
+import os
+import psycopg2
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def init_db():
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS campaigns (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
 try:
     BASE_DIR = Path(__file__).parent
 except NameError:
